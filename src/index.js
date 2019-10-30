88653918b927e433e25730acbe445ccb30567b8b
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-10-30 19:58:01 +0700

    Converting HTML to JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // import the React and ReactDOM libraries
-
 import React from "react";
 import ReactDOM from "react-dom";
 
 // create a react component
 const App = () => {
-    return <div>Hello World!</div>;
+    return (
+        <div>
+            {/* giving class name and reference with jsx */}
+            <label className="label" htmlFor="name">
+                Enter your name:
+            </label>
+            <input id="name" type="text" />
+            {/* inline styling with jsx */}
+            <button style={{ backgroundColor: "blue", color: "white" }}>
+                Submit
+            </button>
+        </div>
+    );
 };
 
 // take the react component and show it on the screen
